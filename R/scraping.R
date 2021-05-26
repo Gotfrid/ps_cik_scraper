5904253303c72bf4492c1fef7205f77b7daa2257
Pavel <paveldemin@me.com>
2021-05-26 22:50:43 +0300

    some refactoring

--- R/scraping.R
+++ R/scraping.R
@@ -2,6 +2,7 @@ library(rvest)
 library(data.table)
 library(stringr)
 library(crayon)
+library(glue)
 
 # === Read the list of region URL === #
 url = "http://cikrf.ru"
@@ -23,11 +24,16 @@ region_addresses <-
                      ][region_name != ""
                        ][!duplicated(region_name)]
 
+# extract short name of region
 region_addresses[, region_short := tstrsplit(region_url, "\\.", keep = 2)]
+
+# url for election sites - mediator url
 region_addresses[, election_url := gsub(pattern = ".izbirkom.ru", 
                                         replacement = ".vybory.izbirkom.ru", 
                                         x = region_url, 
                                         fixed = T)]
+
+# url with the actual information of election comission
 region_addresses[, ps_url := paste(election_url, 
                                    "region", 
                                    region_short,
@@ -51,13 +57,17 @@ for (i in 1:nrow(region_addresses)) {
         .[3] %>% # should be 3 elements always
         as.character()
 
-    region_addresses[i, vrn := tik_id]
-    region_addresses[i, reg_num := region_num]
+    set(region_addresses, i, "vrn", tik_id)
+    set(region_addresses, i, "reg_num", region_num)
+    Sys.sleep(1)
 }
 
 # acquire region children - TIK
+# AGAIN: no need to change anything in URL except region number
 vrn = region_addresses$vrn[1]
 ps_url = region_addresses$ps_url[1]
+
+# init empty list to store tables
 datalist = list()
 
 for (i in 1:nrow(region_addresses)) {
@@ -66,24 +76,35 @@ for (i in 1:nrow(region_addresses)) {
     print(reg_name)
     
     url = glue::glue("{ps_url}Tree&region={reg_num}&vrn={vrn}")
+    
+    # this specific way of reading data is used
+    # to provide easy encoding control
     page <- read_html(url, encoding = "CP1251")
     region_children <- page %>% 
-        html_node("body > p") %>% 
         html_text() %>% 
-        jsonlite::fromJSON(flatten = F) %>% 
+        jsonlite::fromJSON() %>% 
         .$children %>% 
         .[[1]] %>% 
         .[, c("id", "text")]
     datalist[[reg_name]] <- region_children
+    
+    Sys.sleep(1)
 }
 
+# combine all data from the list
 all_tik <- rbindlist(datalist, idcol = "region_name")
 all_tik <- merge(all_tik,
                  region_addresses[, .(region_name, reg_num)],
                  all.x = T)
+# create unique name to transform it later into columns
 all_tik[, unique_name := paste0(region_name, "___", text)]
 
+# Prepare to scrape TIK children
+# init empty list
 datalist_uik = list()
+
+# here we actually need to change stuff in URL except for base url
+# ironically, if base url is changed, scraping does not work
 for (i in 1:nrow(all_tik)) {
     ps_url = region_addresses$ps_url[1]
     vrn = all_tik$id[i]
@@ -92,34 +113,38 @@ for (i in 1:nrow(all_tik)) {
     tik_name = all_tik$text[i]
     unique_name = all_tik$unique_name[i]
     
-    cat(yellow(i), bgBlack(white(reg_name)), " --- ", tik_name, " --- ", vrn,"\n")
+    # could go without crayon, but this loop is for >90k iterations....
+    # TODO: need to log this to a file actually
+    cat(yellow(i), bgBlack(white(reg_name)), " --- ", tik_name, "\n")
     
     url = glue::glue("{ps_url}Tree&region={reg_num}&vrn={vrn}&onlyChildren=true")
     page <- read_html(url, encoding = "CP1251")
-    tik_children <- page %>% 
-        html_node("body > p") %>% 
+    tik_children <- page %>%
         html_text() %>% 
-        jsonlite::fromJSON(flatten = F)
+        jsonlite::fromJSON()
     if (!length(tik_children)) {
         cat(red("NO DATA"), "\n")
         datalist_uik[[unique_name]] <- data.frame(id = NA, text = NA)
-        next    
+        next
     }
     
     datalist_uik[[unique_name]] <- tik_children[, c("id", "text")]
-    Sys.sleep(2)
+    Sys.sleep(1)
 }
 
+# combine data
 all_uik <- rbindlist(datalist_uik, idcol = "unique_name")
 all_uik[, c("region_name", "tik") := tstrsplit(unique_name, "___")]
 all_uik[, unique_name := NULL]
 setnames(all_uik, "text", "uik")
+
+# add url with commission info
+# gonna add UIK ids to it
 all_uik <-
     merge(all_uik,
           region_addresses[, .(region_name, ps_url)],
           all.x = T,
           by = "region_name")
-
 all_uik[, url := paste0(ps_url, "&vrn=", id)]
 all_uik[, ps_url := NULL]
 setcolorder(all_uik, c("region_name", "tik", "uik", "id", "url"))
@@ -140,21 +165,21 @@ for (i in 1:nrow(all_uik)) {
     page <- read_html(all_uik$url[i], encoding = "CP1251")
     
     # all nodes
-    paragrapsh <- page %>% 
+    paragraphs <- page %>% 
         html_nodes(xpath = "//p") %>% 
         html_text(trim = T)
     
     # address
-    address <- paragrapsh[like(paragrapsh, "Адрес комиссии:")]
+    address <- paragraphs[like(paragraphs, "Адрес комиссии:")]
     
     # phone
-    phone <- paragrapsh[like(paragrapsh, "Телефон:")]
+    phone <- paragraphs[like(paragraphs, "Телефон:")]
     
     # email
-    email <- paragrapsh[like(paragrapsh, "Адрес электронной почты:")]
+    email <- paragraphs[like(paragraphs, "Адрес электронной почты:")]
     
     # duty end
-    duty_end <- paragrapsh[like(paragrapsh, "Срок окончания полномочий:")]
+    duty_end <- paragraphs[like(paragraphs, "Срок окончания полномочий:")]
     
     # map
     coordinates <- page %>% 
@@ -162,14 +187,12 @@ for (i in 1:nrow(all_uik)) {
         html_attrs() %>% 
         .[c("coordlat", "coordlon")]
     
-    all_uik[i, `:=`(
-        uik_address = address,
-        uik_phone = phone,
-        uik_email = email,
-        uik_duty_end = duty_end,
-        uik_lon = coordinates['coordlon'],
-        uik_lat = coordinates['coordlat']
-    )]
+    set(all_uik, i, "uik_address", address)
+    set(all_uik, i, "uik_phone", phone)
+    set(all_uik, i, "uik_email", email)
+    set(all_uik, i, "uik_duty_end", duty_end)
+    set(all_uik, i, "uik_lon", coordinates['coordlon'])
+    set(all_uik, i, "uik_lat", coordinates['coordlat'])
     
     # time interval is an ethical MUST
     Sys.sleep(1)
